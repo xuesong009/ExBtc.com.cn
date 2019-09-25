623d363695c9292103dc13ef0e456c827b3221db
xuesong009 <1687283227@qq.com>
2019-09-25 16:29:21 +0800

    Index.vue

---
+++ src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="index">
+    <!-- banner start -->
+    <Banner/>
+    <!-- banner end -->
+
+    <!-- footer start -->
+    <Footer/>
+    <!-- footer end -->
+  </div>
+</template>
+
+<script>
+  import Banner from '../components/Banner'
+  import Footer from '../components/Footer'
+
+  export default {
+    name: "Index",
+    data() {
+      return {};
+    },
+    components: {
+      Banner,
+      Footer
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
+
+<style scoped lang="less">
+.index{
+  width: 100%;
+  position: relative;
+}
+</style>
