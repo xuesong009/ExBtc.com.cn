7fa8d6b46379c5dc6da46fd90d1abed0235ed2be
xuesong009 <1687283227@qq.com>
2019-09-25 16:34:46 +0800

    App.vue

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Nav/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import Nav from './components/Nav'
+
+  export default {
+    name: 'App',
+    data() {
+      return {}
+    },
+    components: {
+      Nav
+    }
+  }
+</script>
+
+<style lang="less">
+  /* 引入resetCss less */
+  @import "./assets/less/resetCss";
+  /*引入common less*/
+  @import "./assets/less/common";
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+</style>
