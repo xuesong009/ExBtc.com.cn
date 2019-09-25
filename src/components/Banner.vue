0af300c23503b02238f2085ec783aca9b7033fb9
xuesong009 <1687283227@qq.com>
2019-09-25 16:27:31 +0800

    Banner.vue

---
+++ src/components/Banner.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="banner">
+    <ul>
+      <li class="banner-list"><a href=""></a></li>
+      <li class="banner-list"><a href=""></a></li>
+      <li class="banner-list"><a href=""></a></li>
+    </ul>
+    <div class="dots">
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "banner"
+  }
+</script>
+
+<style scoped lang="less">
+  .banner {
+    width: 100%;
+    height:450px;
+    background: url("../assets/images/banner-bg.jpg") no-repeat;
+    /*background-size: 800px 450px;*/
+    /*border: 1px solid blue;*/
+    > ul {
+      width: 300%;
+      list-style: none;
+      > .banner-list {
+        > a {
+
+        }
+      }
+    }
+  }
+</style>
