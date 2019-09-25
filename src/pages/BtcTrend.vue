3d0de14961900cd8930a287eba4b30f542123d94
xuesong009 <1687283227@qq.com>
2019-09-25 16:30:34 +0800

    BtcTrend.vue

---
+++ src/pages/BtcTrend.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrap">
+    btc 趋势
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "BtcTrend"
+  }
+</script>
+
+<style scoped>
+  .wrap {
+    width: 800px;
+    border:1px solid red;
+    margin: 0 auto;
+    font-family: Consolas,sans-serif;
+    font-size: 25px;
+    color: red;
+  }
+</style>
