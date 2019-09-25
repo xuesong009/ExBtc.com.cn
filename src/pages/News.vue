4601914582d7df877302a57e7690e67c03dd8dd6
xuesong009 <1687283227@qq.com>
2019-09-25 16:29:46 +0800

    News.vue

---
+++ src/pages/News.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrap">
+    btc News
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "News"
+  }
+</script>
+
+<style scoped>
+  .wrap {
+    width: 800px;
+    border:1px solid red;
+    margin: 0 auto;
+    font-family: Consolas,sans-serif;
+    font-size: 25px;
+    color: red;
+  }
+</style>
